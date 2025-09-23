490d57dba5ce4be6da98a8bc180f1550b52d0e41
Hove <beluchukwu755@gmail.com>
2025-09-23 22:24:37 +0100

    fixed a bug that caused the  button to not return the modal form

--- script.js
+++ script.js
@@ -6,6 +6,29 @@
         element.classList.add('selected');
     }
 
-    document.getElementById('float-btn').addEventListener('click', () => {
-        alert('new expense clicked')
-    })
+    document.getElementById('add-btn').addEventListener('click', () => {
+             alert('Add new expense clicked!');
+});
+
+
+
+    const addBtn = document.getElementById('add-btn');
+     const modal = document.getElementById('expense-modal');
+     const closeBtn = document.getElementById('close-modal');
+
+
+addBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
+
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
